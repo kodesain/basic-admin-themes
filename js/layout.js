31d3f301a16dd5a7ac2ae4e45fc75e43075dec5c
Romi A Hakim <romi.hakim@eaciit.com>
2018-12-08 09:47:28 +0700

    Basic Admin Themes

--- js/layout.js
+++ js/layout.js
@@ -1,34 +1,34 @@
 "use strict";
 
 var closeNavigate = function () {
-    $("body").removeClass("ks-opened-sidebar"), $("body").removeClass("ks-body-fixed")
+    $("body").removeClass("app-opened-sidebar"), $("body").removeClass("app-body-fixed")
 }, openNavigate = function () {
-    $(window).width() < 760 && $("body").addClass("ks-body-fixed"), $("body").addClass("ks-opened-sidebar")
+    $(window).width() < 760 && $("body").addClass("app-body-fixed"), $("body").addClass("app-opened-sidebar")
 }, addWaveEffect = function (e, a) {
-    var s = ".ks-wave-effect", n = e.outerWidth(), t = a.offsetX, i = a.offsetY;
-    e.prepend('<span class="ks-wave-effect"></span>'), $(s).css({top: i, left: t}).animate({opacity: "0", width: 2 * n, height: 2 * n}, 500, function () {
+    var s = ".app-wave-effect", n = e.outerWidth(), t = a.offsetX, i = a.offsetY;
+    e.prepend('<span class="app-wave-effect"></span>'), $(s).css({top: i, left: t}).animate({opacity: "0", width: 2 * n, height: 2 * n}, 500, function () {
         e.find(s).remove()
     })
 };
 
 $(document).ready(function () {
-    $(".ks-header-submenu").show(), $(".ks-overlay, .ks-sidebar-toggle-button").on("click", function () {
+    $(".app-header-submenu").show(), $(".app-overlay, .app-sidebar-toggle-button").on("click", function () {
         closeNavigate()
-    }), $(".ks-toggle-sidebar").on("click", function () {
-        $("body").hasClass("ks-opened-sidebar") ? closeNavigate() : openNavigate()
-    }), $(".ks-sidebar-pin-button").on("click", function () {
-        $("body").toggleClass("ks-pinned-sidebar")
-    }), $(".ks-search-toggle").on("click", function (e) {
-        e.preventDefault(), $(".ks-search-bar").toggleClass("active")
-    }), $(".ks-header-submenu").parent().find("a:first").on("click", function (e) {
-        e.stopPropagation(), e.preventDefault(), $(this).parents(".ks-header-navigation").find(".ks-header-submenu").not($(this).parents("li").find(".ks-header-submenu")).removeClass("active"), $(this).parents("li").find(".ks-header-submenu").show().toggleClass("active")
-    }), $(".ks-sidebar-navi li.show > ul").slideDown(200), $(".ks-sidebar-navi a").on("click", function (e) {
+    }), $(".app-toggle-sidebar").on("click", function () {
+        $("body").hasClass("app-opened-sidebar") ? closeNavigate() : openNavigate()
+    }), $(".app-sidebar-pin-button").on("click", function () {
+        $("body").toggleClass("app-pinned-sidebar")
+    }), $(".app-search-toggle").on("click", function (e) {
+        e.preventDefault(), $(".app-search-bar").toggleClass("active")
+    }), $(".app-header-submenu").parent().find("a:first").on("click", function (e) {
+        e.stopPropagation(), e.preventDefault(), $(this).parents(".app-header-navigation").find(".app-header-submenu").not($(this).parents("li").find(".app-header-submenu")).removeClass("active"), $(this).parents("li").find(".app-header-submenu").show().toggleClass("active")
+    }), $(".app-sidebar-navi li.show > ul").slideDown(200), $(".app-sidebar-navi a").on("click", function (e) {
         var a = $(this);
         $(this).next().is("ul") ? (e.preventDefault(), a.parent().hasClass("show") ? (a.parent().removeClass("show"), a.next().slideUp(200)) : (a.parent().parent().find(".show ul").slideUp(200), a.parent().parent().find("li").removeClass("show"), a.parent().toggleClass("show"), a.next().slideToggle(200))) : (a.parent().parent().find(".show ul").slideUp(200), a.parent().parent().find("li").removeClass("show"), a.parent().addClass("show"))
-    }), $(".ks-material-button").on("click", function (e) {
+    }), $(".app-material-button").on("click", function (e) {
         addWaveEffect($(this), e)
     }), $(document).on("click", function (e) {
         var a = $(e.target);
-        !0 === $(".ks-header-submenu").hasClass("active") && !a.hasClass("ks-submenu-toggle") && a.parents(".ks-header-submenu").length < 1 && $(".ks-header-submenu").removeClass("active"), a.parents(".ks-search-bar").length < 1 && !a.hasClass("ks-search-bar") && !a.parent().hasClass("ks-search-toggle") && !a.hasClass("ks-search-toggle") && $(".ks-search-bar").removeClass("active")
+        !0 === $(".app-header-submenu").hasClass("active") && !a.hasClass("app-submenu-toggle") && a.parents(".app-header-submenu").length < 1 && $(".app-header-submenu").removeClass("active"), a.parents(".app-search-bar").length < 1 && !a.hasClass("app-search-bar") && !a.parent().hasClass("app-search-toggle") && !a.hasClass("app-search-toggle") && $(".app-search-bar").removeClass("active")
     })
 });
